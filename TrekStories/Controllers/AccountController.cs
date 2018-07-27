668065515cf0d42e77c6027709e12c0f2bfb87a3
Carole vd <Carole.vanDamme@outlook.com>
2018-07-27 21:17:14 +0100

    Username Inputted by User on Registration

--- TrekStories/Controllers/AccountController.cs
+++ TrekStories/Controllers/AccountController.cs
@@ -73,9 +73,16 @@ namespace TrekStories.Controllers
                 return View(model);
             }
 
+            string user_name = ""; // in case 'user' is null (user not found)
+            var user = await UserManager.FindByEmailAsync(model.Email);
+
+            if (user != null)
+            {
+                user_name = user.UserName;
+            }
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(user_name, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,7 +158,7 @@ namespace TrekStories.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
