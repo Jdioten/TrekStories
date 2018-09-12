61b6d03b2d2b301c1e8637f9423043c0a7c57a03
Carole vd <Carole.vanDamme@outlook.com>
2018-09-12 22:10:30 +0100

    Created Blob Utility class with reference to Azure blob storage libraries

---
+++ TrekStories/Utilities/BlobUtility.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+
+namespace TrekStories.Utilities
+{
+    public class BlobUtility
+    {
+    }
+}
