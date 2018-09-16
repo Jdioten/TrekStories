5e2a0ddafbb4deeba93d02e7e58537cc13232cd2
Carole vd <Carole.vanDamme@outlook.com>
2018-09-16 14:26:37 +0100

    Added methods to Blob utility class to get container, upload and delete blob

--- TrekStories/Utilities/BlobUtility.cs
+++ TrekStories/Utilities/BlobUtility.cs
@@ -2,10 +2,55 @@
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System;
+using System.IO;
+using System.Web;
 
 namespace TrekStories.Utilities
 {
     public class BlobUtility
     {
+        private CloudBlobContainer GetCloudBlobContainer(string containerName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                    CloudConfigurationManager.GetSetting("trekstoriesblobstorage_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            return container;
+        }
+
+        public CloudBlockBlob UploadBlob(string blobName, string containerName, Stream stream)
+        {
+            CloudBlobContainer container = GetCloudBlobContainer(containerName);
+            container.CreateIfNotExists();
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobName);
+
+            try
+            {
+                blockBlob.UploadFromStream(stream);
+                return blockBlob;
+            }
+            catch (Exception e)
+            {
+                var r = e.Message;
+                return null;
+            }
+        }
+
+        public void DeleteBlob(string blobName, string containerName)
+        {
+            CloudBlobContainer container = GetCloudBlobContainer(containerName);
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobName);
+            blockBlob.DeleteIfExists();
+        }
+
+
+        //This method will most probably not be required as it can be downloaded straight from URL
+        //public CloudBlockBlob DownloadBlob(string blobName, string containerName)
+        //{
+        //    CloudBlobContainer container = GetCloudBlobContainer(containerName);
+        //    CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobName);
+        //    // blockBlob.DownloadToStream(Response.OutputStream);
+        //    return blockBlob;
+        //}
     }
 }
