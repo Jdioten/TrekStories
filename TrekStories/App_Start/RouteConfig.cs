22ed08beb3d1f5d6ccdb2f0c88d701f550a41ffb
Carole vd <Carole.vanDamme@outlook.com>
2018-09-29 11:43:31 +0100

    Added 2 new routes for more user-friendly URLs

--- TrekStories/App_Start/RouteConfig.cs
+++ TrekStories/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace TrekStories
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Step Creation",
+                url: "Create/onTrip{tripId}/Step{seqNo}",
+                defaults: new { Controller = "Step", action = "Create" }
+            );
+
+            routes.MapRoute(
+                name: "Accommodation List",
+                url: "Accommodation/Index/{tripId}/{sortOrder}",
+                defaults: new { Controller = "Accommodation", action = "Index", sortOrder = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
