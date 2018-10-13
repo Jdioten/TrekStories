6d8051bd9df9586366d394724854afe3c6aa25dd
Carole vd <Carole.vanDamme@outlook.com>
2018-10-13 22:44:22 +0100

    Routes Unit Tests

---
+++ TrekStories.Tests/RouteTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Reflection;
+using System.Web;
+using System.Web.Routing;
+
+namespace TrekStories.Tests
+{
+    [TestClass]
+    public class RouteTests
+    {
+        [TestMethod]
+        public void TestDefaultIncomingRoutes()
+        {
+            // check for the URL that is hoped for
+            TestRouteMatch("~/Home/Index", "Home", "Index");
+            TestRouteMatch("~/Home", "Home", "Index");
+            // check that the values are being obtained from the segments
+            TestRouteMatch("~/One/Two", "One", "Two");
+            // ensure that too many segments fails to match
+            TestRouteFail("~/Home/Index/Segment1/Segment2");
+        }
+
+        [TestMethod]
+        public void TestCustomRoutes()
+        {
+            // check step creation URL
+            TestRouteMatch("~/Create/onTrip1/Step2", "Step", "Create", new { tripId = "1", seqNo = "2" });
+            TestRouteMatch("~/Accommodation/Index/1/name_desc", "Accommodation", "Index", new { tripId = "1", sortOrder = "name_desc"}, "GET");
+        }
+
+        //copied from book Pro ASP.NET MVC 5 by Adam Freeman
+        private HttpContextBase CreateHttpContext(string targetUrl = null, string httpMethod = "GET")
+        {
+            // create the mock request
+            Mock<HttpRequestBase> mockRequest = new Mock<HttpRequestBase>();
+            mockRequest.Setup(m => m.AppRelativeCurrentExecutionFilePath)
+                .Returns(targetUrl);
+            mockRequest.Setup(m => m.HttpMethod).Returns(httpMethod);
+            // create the mock response
+            Mock<HttpResponseBase> mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(m => m.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(s => s);
+            // create the mock context, using the request and response
+            Mock<HttpContextBase> mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(m => m.Request).Returns(mockRequest.Object);
+            mockContext.Setup(m => m.Response).Returns(mockResponse.Object);
+            // return the mocked context
+            return mockContext.Object;
+        }
+
+        public void TestRouteMatch(string url, string controller, string action, object routeProperties = null, string httpMethod = "GET")
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            // Act - process the route
+            RouteData result = routes.GetRouteData(CreateHttpContext(url, httpMethod));
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(TestIncomingRouteResult(result, controller, action, routeProperties));
+        }
+
+        private bool TestIncomingRouteResult(RouteData routeResult, string controller, string action, object propertySet = null)
+        {
+            Func<object, object, bool> valCompare = (v1, v2) => {
+                return StringComparer.InvariantCultureIgnoreCase
+                .Compare(v1, v2) == 0;
+            };
+            bool result = valCompare(routeResult.Values["controller"], controller) && valCompare(routeResult.Values["action"], action);
+            if (propertySet != null)
+            {
+                PropertyInfo[] propInfo = propertySet.GetType().GetProperties();
+                foreach (PropertyInfo pi in propInfo)
+                {
+                    if (!(routeResult.Values.ContainsKey(pi.Name) && valCompare(routeResult.Values[pi.Name], pi.GetValue(propertySet, null))))
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public void TestRouteFail(string url)
+        {
+            // Arrange
+            RouteCollection routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            // Act - process the route
+            RouteData result = routes.GetRouteData(CreateHttpContext(url));
+            // Assert
+            Assert.IsTrue(result == null || result.Route == null);
+        }
+    }
+}
