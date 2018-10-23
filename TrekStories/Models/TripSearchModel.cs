7993e336850bfe6cf1d3d2b6103d92d37eab1e1f
Carole vd <Carole.vanDamme@outlook.com>
2018-10-23 23:12:02 +0100

    SearchForm Partial View

--- TrekStories/Models/TripSearchModel.cs
+++ TrekStories/Models/TripSearchModel.cs
@@ -1,11 +1,20 @@
-﻿namespace TrekStories.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrekStories.Models
 {
     public class TripSearchModel
     {
+        [Display(Name = "Title Keyword")]
         public string TitleKeyword { get; set; }
         public string Country { get; set; }
+        [Display(Name = "Trip Category")]
         public TripCategory? TripCategory { get; set; }
+        [Display(Name = "Minimum Duration")]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Please enter a number above 0.")]
         public int? MinDuration { get; set; }
+        [Display(Name = "Maximum Duration")]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Please enter a number above 0.")]
         public int? MaxDuration { get; set; }
     }
 }
