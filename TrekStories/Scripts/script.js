cccaf9b60e6d5fcb5ec3e2d5a803fb6e42441379
Carole vd <Carole.vanDamme@outlook.com>
2018-10-29 14:21:09 +0000

    Javascript to check Min & Max Durations on Search form

---
+++ TrekStories/Scripts/script.js
@@ -0,0 +1,14 @@
+﻿////validate min and max durations on Search Form		
+$(document).ready(function () {
+    $('#SearchForm').on('submit', function (e) {
+
+        // Check min <= max
+        if ($('#MinDuration').val() != '' && $('#MaxDuration').val() != '' && $('#MinDuration').val() > $('#MaxDuration').val())
+        {
+            $("#minDurationValidation").append("The minimum duration cannot be bigger than the maximum duration.");
+
+        // Stop submission of the form
+        e.preventDefault();
+        }
+    });
+});
